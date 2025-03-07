0a37ce1382a13ea0f679917e1a01fda67eb3669c
MKMKMWWWW <qq644694530@gmail.com>
2025-03-07 17:42:36 +0800

    2025/3/7_2

--- src/pages/exchange_rate.py
+++ src/pages/exchange_rate.py
@@ -25,6 +25,7 @@ def parse_exchange_rates(html):
     try:
  
         soup = BeautifulSoup(html, "html.parser")
+        #找对应标头
         p_element = soup.find('p', string=lambda text: text and "现汇卖出价" in text)
         next_p = p_element.find_next_sibling('p') if p_element else None
 
@@ -54,7 +55,7 @@ def fetch_dxy(url):
 
         time.sleep(5)
         
-
+        #找对应标头
         dxy_element = driver.find_element(By.CSS_SELECTOR, "span.last-JWoJqCpY.js-symbol-last")
         dxy_value = dxy_element.text
        
@@ -69,6 +70,7 @@ def fetch_dxy(url):
     return dxy_value,change_value,percentage_change
 
 
+
 def show():
     st.title("汇率")  
     url1 = "https://www.tradingview.com/symbols/TVC-DXY/"#tradingview
@@ -76,14 +78,35 @@ def show():
     url3 = "https://chl.cn/huilv/?usd"#我查
     dxy,dxy1,dxy2 = fetch_dxy(url1)
     usd_br,usd_br_1,usd_br_2 = fetch_dxy(url2)
-    st.metric(label="美元指数", value=dxy)
-    st.write(dxy1,dxy2)
-    st.metric(label="美元对巴西雷亚尔", value=usd_br)
-    st.write(usd_br_1,usd_br_2)
+    st.metric(label="### 美元指数", value=dxy)
+    st.write(dxy1,dxy2,"(对应昨天的数据)")
+    st.metric(label="### 美元对巴西雷亚尔", value=usd_br)
+    st.write(usd_br_1,usd_br_2,"(对应昨天的数据)")
     exchange_rates = parse_exchange_rates(url3)
-    st.metric(label="美元对人名币（现汇卖出价）", value=exchange_rates)
+    st.metric(label="### 美元对人名币（现汇卖出价）", value=exchange_rates)
+    # 示例输入
+    st.write('---')
+    tit2, tit3 ,tit4 ,tit5 = st.columns([1, 1, 1, 1])
+    with tit2:
+        futures_price = st.number_input("### 请输入期货价格: (吨)", min_value=0.0)
+    with tit3:
+        fee_price = st.number_input("### 请输入杂费: （默认+500杂费)", min_value=0.0,value=500.0)
+    with tit4:
+        intax_price = st.number_input("### 请输入进口税: (默认1.12)", min_value=0.0,value=1.12)
+    with tit5:
+        outtax_price = st.number_input("### 请输入期货价格: (默认1.09)", min_value=0.0,value=1.09)
+    # 计算现货价格
+    spot_price = futures_price*intax_price*outtax_price*float(exchange_rates)+fee_price
+    st.write(f"### 现货价格为: {spot_price/1000:.2f}元/千克")
+    st.write('---')
+    # 计算转换价格
+    futures_change_price = st.number_input("### 请输入雷亚尔价格: ", min_value=0.0)
+    change_price = futures_change_price/float(usd_br)/15
+    st.write(f"### 转换成美元后价格为: {change_price:.2f}美元/千克")
 
-    if st.button("Refresh Now"):
+    #刷新数据
+    if st.button("刷新数据"):
         st.cache_data.clear()  # Clear the cache data
         st.rerun()  # Trigger a rerun
+    
 
